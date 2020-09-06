06334d043103833edc32ecc3dfa83b95d93274e4
Ayrton Domingos Lorenceti <ayrtonn_smo@hotmail.com>
2020-09-06 18:19:24 -0300

    [06-09-2020] Teste de logs. Adição de logs no dynamodb_model.

--- models/dynamodb_model.py
+++ models/dynamodb_model.py
@@ -1,6 +1,7 @@
 import boto3
 
 from boto3.dynamodb.conditions import Key
+from loguru import logger
 
 class DynamoDB:
   dynamodb = boto3.resource('dynamodb')
@@ -10,6 +11,10 @@ class DynamoDB:
   
   @staticmethod
   def search(self, table_name, key, value):
+    logger.info("### DynamoDB.search(table_name, key, value) ###")
+    logger.info("table_name: {} - key: {} - value: {}".format(table_name, key, value))
+    logger.info("### DynamoDB.search(table_name_ key, value) ###")
+
     table = self.dynamodb.Table(table_name)
 
     search_response = table.query( 
@@ -20,6 +25,10 @@ class DynamoDB:
   
   @staticmethod
   def put(self, table_name, item):
+    logger.info("### DynamoDB.put(table_name, key, value) ###")
+    logger.info("table_name: {} - item: {}".format(table_name, item))
+    logger.info("### DynamoDB.put(table_name, key, value) ###")    
+
     table = self.dynamodb.Table(table_name)
 
     table.put_item(Item=item)
