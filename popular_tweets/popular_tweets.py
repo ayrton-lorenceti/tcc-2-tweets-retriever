1c81ce8b5e4287edbb7f12e2f708879bec89e6e6
Ayrton Domingos Lorenceti <ayrtonn_smo@hotmail.com>
2020-09-05 21:41:21 -0300

    [05-09-2020] Adicionado logs no popular_tweets.

--- popular_tweets/popular_tweets.py
+++ popular_tweets/popular_tweets.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+from loguru import logger
 
 from models.dynamodb_model import DynamoDB
 from models.search_metadata_model import SearchMetadata
@@ -6,6 +7,8 @@ from models.tweepy_model import Tweepy
 from models.tweet_model import Tweet
 
 def retrieve_tweets_by_until_param():
+  logger.info( { "method": "retrieve_tweets_by_until_param()" } )
+
   # Set initial condition based on Lambda
   until_param = date.today() - timedelta(days = 7)
 
@@ -21,6 +24,8 @@ def retrieve_tweets_by_until_param():
   SearchMetadata.save_since_id(search_results["search_metadata"]["next_results"])
 
 def retrieve_tweets_by_result_type():
+  logger.info( { "method": "retrieve_tweets_by_result_type()" } )
+
   # Search since_id based on result_type
   since_id = DynamoDB.search(DynamoDB, "Search_Metadata", "result_type", "popular").pop()["since_id"]
 
